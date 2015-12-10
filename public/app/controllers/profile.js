10d7ea452177f335c7c77661c5f3e1c3c40afdeb
way0750 <bruce0750@gmail.com>
2015-12-10 15:59:47 -0800

    added template for profile for displaying traits and up down voting

--- public/app/controllers/profile.js
+++ public/app/controllers/profile.js
@@ -5,16 +5,20 @@
   .controller('profileCtrl', ['$scope', '$http','dashboardService', '$location', function ($scope, $http, dashboardService, $location) {
 
     $scope.targetUserId = sessionStorage.getItem('targetUserId');
-
+    $scope.pubUserData = {
+      awesomeness: 1000,
+      handsomeness: 2000,
+      iq: 3000
+    };
     $scope.getUserData = function (userId) {
 
       return dashboardService.getUserData(userId)
       .then(function (data) {
-        $scope.pubUserData = data;
+        // $scope.pubUserData = data;
         return data;
       })
       .catch(function (data) {
-        $scope.pubUserData = false;
+        // $scope.pubUserData = false;
         return false;
       });
     };
