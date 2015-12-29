d85ee1839db4101895e72e1b1f75d560cc2f4a1d
Andrew Deal <deal.e.andrew@gmail.com>
2015-12-29 11:44:08 -0800

    Configures jsdoc grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,39 @@
 module.exports = function (grunt) {
-	
+	grunt.initConfig({
+		
+		watch : {
+			files : ["README.md"],
+		    tasks : ["holla"]
+		},
+		concat : {
+
+		},
+		uglify : {
+
+		},
+		jsdoc : {
+
+			dist : {
+				src     : [
+						   'public/app/*.js', 
+						   'public/app/**/*.js',
+						   'server/*.js',
+						   'server/**/*.js'
+						  ],
+				options : {
+					destination : 'doc',
+				}
+			}
+
+		}
+	});
+
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-jsdoc");
+
+	grunt.registerTask('holla', function() {
+		console.log('Setting up grunt');
+	});
 }
